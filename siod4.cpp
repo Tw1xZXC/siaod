308ad796a27b5094d3dc3a1723918034cf90bc6a
Tw1xZXC <100905908+Tw1xZXC@users.noreply.github.com>
2022-05-30 10:51:22 +0300

    Create siod4.cpp

---
+++ siod4.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+//ф-ция поиска
+int barrierSearch(int *x, int n, int key){
+    x[n]=key;
+    int i=0;
+    while(x[i]!=key)i++;
+    return i;
+}
+
+int main() {
+    //объявление переменных
+    int key, n, i, j, buff;
+    
+    //ввод размера массива
+    cout<<"array size:";
+    cin>>n;
+    
+    //динамический массив + его заполнение
+    int* arr=new int[n];
+    for (int i=0;i<n;i++)
+    {
+        arr[i]=rand()%10000;
+    }
+    cout<<endl;
+    
+    //вывод массива
+    for (int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    //ключ
+    cout<<endl<<"key: ";
+    cin>>key;
+    
+    cout<<"index of "<<key<<" is "<<barrierSearch(arr,n,key)+1;
+    return 0;
+}
